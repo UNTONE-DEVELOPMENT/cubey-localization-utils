0f1217f4ae2dfaa16522aca1b354607fb121769f
BuildTools <33783503+Hubza@users.noreply.github.com>
2021-10-19 09:26:58 +0100

    Update Localization.cs
    Co-Authored-By: dusstuiter <72349963+dusstuiter@users.noreply.github.com>

--- Localization.cs
+++ Localization.cs
@@ -20,7 +20,7 @@ public static class Localization
 
     public static List<localeString> localeStrings;
     public static string[] folders = { "game", "general" };
-    public static string[] locales = { "en_GB", "ja_JP" };
+    public static string[] locales = { "en_GB", "nl_NL" };
     public static void LoadLocales()
     {
         localeStrings = new List<localeString>();
